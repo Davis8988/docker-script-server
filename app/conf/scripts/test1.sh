2334f6e9a92b6b3c5dd278aed576560abd33d79b
David Yair <david.yair@mail.com>
2025-05-27 10:35:17 +0300

    echo "Hello World" > hello.txt

--- app/conf/scripts/test1.sh
+++ app/conf/scripts/test1.sh
@@ -2,6 +2,18 @@
 echo $1
 echo $2
 
+echo PWD=$(pwd)
+
+echo mkdir results
+mkdir results
+echo cd results
+cd results
+echo pwd
+pwd
+echo ls -l
+ls -l
+echo echo "Hello World" > hello.txt
+echo "Hello World" > hello.txt
 
 touch david.txt
 echo $(date) > david.txt
